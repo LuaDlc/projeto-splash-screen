a28861b5d12f1f0e1f7c24b281206225b390b9ef
LuaDlc <luadiaslc@gmail.com>
2022-08-01 15:24:23 -0300

    criando uma implementação usando api dio

--- projeto_splash_screen/lib/pages/home_page.dart
+++ projeto_splash_screen/lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_splash_screen/repositories/home_repository_imp.dart';
 
 import '../controllers/home_controller.dart';
 import '../models/post_model.dart';
-import '../repositories/home_repository_mock.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,7 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  final HomeController _controller = HomeController(HomeRepositoryMock());
+  final HomeController _controller = HomeController(HomeRepositoryImp());
 
   @override
   void initState() {
@@ -24,14 +24,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
       body: ValueListenableBuilder<List<PostModel>>(
         valueListenable: _controller.posts,
         builder: (_, list, __) {
-          return ListView.builder(
+          return ListView.separated(
+            shrinkWrap: true,
             itemCount: list.length,
             itemBuilder: (_, idx) => ListTile(
+              leading: Text(list[idx].id.toString()),
               title: Text(list[idx].title),
+              trailing: Icon(Icons.arrow_forward),
             ),
+            separatorBuilder: (_, __) => Divider(),
             );
         }
       ),
